a541eaccca3615e6b19950a4d6b7dcbaba331a14
dheena5248D <dheenadhayalan5248d@gmail.com>
2023-06-16 21:54:07 +0530

    about me page v1.0

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ return(
   <div id="navdiv">
     <span id="dd">DD</span>
     <nav className="nav">
-      <li className="navbutt" href="" >home</li>
-      <li className="navbutt" href="" >aboutMe</li>
+      <li className="navbutt" href="#home" >home</li>
+      <li className="navbutt" href="#abotme" >aboutMe</li>
       <li className="navbutt" href="" >hireMe</li>
       <li className="navbutt" href="" >contactMe</li>
     </nav>
@@ -33,11 +33,41 @@ const Home =()=>{
      
   </section>)
 }
+const AboutMe =()=>{
+  return(
+    <div style={{ marginBottom:"0px", marginTop:"40px"}}>
+  <p className="abme">ABOUT ME</p></div>)
+}
+const AboutMeContent =()=>{
+  return(
+< section id="abotme" style={{ display: "flex",  marginTop:"00px" }}>
+<div id="aliit">
+   
+    <li className="abliit">Name:</li>
+    <li className="abliit">Dob:</li>
+    <li className="abliit">Email:</li>
+    <li className="abliit">Nationality:</li>
+  </div>
+  <div id="abandiv">
+    <li className="aban">dheena dayalan</li>
+    <li className="aban">October15,2005</li>
+    <li className="aban">dheenadhayalan5248d@gmail.com</li>
+    <li className="aban">Indian</li>
+
+  </div>
+  <div id="img2div" style={{ marginTop: "-150px" }}>
+     <img id="img2" src="https://i.ibb.co/KrswXb1/userlmn-c4f4ba50d98beb6ab451b6839f7fcf4f.png" alt="dd" border="0" /></div>
+</section>)
+}
+
+
 const App = () => {
   return (
     <div>
     <Nav />
     <Home />
+    <AboutMe />
+    <AboutMeContent />
     </div>
   );
 };
